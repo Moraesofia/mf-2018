9057902f82f88c8c719f78415d01e90fa878ad16
Sofia Moraes <sofiahj@hotmail.com>
2018-09-16 11:50:27 -0300

    Update Nome.java

--- semana/src/br/ufg/inf/mf/pratica/Nome.java
+++ semana/src/br/ufg/inf/mf/pratica/Nome.java
@@ -3,7 +3,7 @@ package br.ufg.inf.mf.pratica;
 import java.time.LocalDate;
 
 //
-public class Aplicacao {
+public class Nome {
 
         public static void main(String[] args) {
         System.out.println(LocalDate.now().getDayOfWeek());
